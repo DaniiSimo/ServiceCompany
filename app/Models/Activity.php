d4bd8018e88b9876a9c68dbd715b070bacc913eb
Danisimo <savenkov-lokin2013@yandex.ru>
2025-10-12 19:38:20 +0300

    Dev init data: Добавлены фабрики, сидр и сервис для начальной инциализации данными
    Description:
    - Для деятельности, здания, организации и телефона были добавлены фабрики и трейты
    - Был реализован сервис, который использует фабрики и заполняет фейковыми данными
    - Для запуска, инициализации данных был взят сидр

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Activity extends Model
 {
+    use HasFactory;
     public function organizations(): BelongsToMany
     {
         return $this->belongsToMany(Organization::class,  'organizations_and_activities');
---
+++ database/factories/ActivityFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Activity;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Activity>
+ */
+class ActivityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function definition(): array
+    {
+        $name = fake()->unique()->words(nb: 2,asText:  true);
+
+        return [
+            'name' => $name,
+            'path' => trim(Str::slug(Str::ascii($name),'_')),
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ];
+    }
+    public function childOf(Activity $parent): self
+    {
+        return $this->state(function (array $attributes) use ($parent) {
+            return [
+                'path' => $parent->path.'.'.trim(Str::slug(Str::ascii($attributes['name']),'_')),
+            ];
+        });
+    }
+
+    public function getActivities(): array
+    {
+        return $this->activities;
+    }
+}
