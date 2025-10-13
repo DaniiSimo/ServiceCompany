72d7f2e3dd70c23305aab5d9d7a0a869bf2cf386
Danisimo <savenkov-lokin2013@yandex.ru>
2025-10-13 16:52:34 +0300

    Dev create api: Добавил описание api

--- app/OpenApi.php
+++ app/OpenApi.php
@@ -3,13 +3,14 @@
 namespace App;
 use OpenApi\Attributes as OA;
 
-#[OA\Info(title: 'My API', version: '1.0.0')]
-#[OA\Server(url: '/')]
+#[OA\Info(title: 'Organization API', version: '1.0.0', contact: new OA\Contact(name: 'Данила', url: 'https://github.com/DaniiSimo'))]
+#[OA\Server(url: 'http/localhost/api')]
 #[OA\SecurityScheme(
     securityScheme: 'bearerAuth',
     type: 'http',
     scheme: 'bearer',
-    bearerFormat: 'Sanctum'
+    bearerFormat: 'Sanctum',
+	description: 'Вводите токен без "Bearer " — UI подставит префикс сам'
 )]
 class OpenApi
 {
