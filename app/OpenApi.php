6553e9934c42225d7edabe136bbb7fcfe314a1a9
Danisimo <savenkov-lokin2013@yandex.ru>
2025-10-16 00:07:15 +0300

    fix: Изменено описание документации api и перенесён интерфейс swagger на home страницу

--- app/OpenApi.php
+++ app/OpenApi.php
@@ -4,7 +4,7 @@ namespace App;
 use OpenApi\Attributes as OA;
 
 #[OA\Info(title: 'Organization API', version: '1.0.0', contact: new OA\Contact(name: 'Данила', url: 'https://github.com/DaniiSimo'))]
-#[OA\Server(url: 'http/localhost/api')]
+#[OA\Server(url: 'http://localhost')]
 #[OA\SecurityScheme(
     securityScheme: 'bearerAuth',
     type: 'http',
